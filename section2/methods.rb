e0ac7305c790f4f898cdadaf157e60eb179c1a67
philipd <phildkh97@gmail.com>
2021-07-06 22:04:52 +0800

    feat(section2): add methods file in section2 folder

---
+++ section2/methods.rb
@@ -0,0 +1,49 @@
+# Section 2 - Methods
+# Flow of Execution of Ruby is Top to Bottom
+# Therefore Methods have to be declared above before calling it
+
+def multiply(num_1, num2)
+    num_1 * num2
+end
+
+def divide(num1, num2)
+    num1 / num2
+end
+
+def subtract(num2, num1)
+    num2 - num1
+end
+
+def add(num2, num1)
+    num2 + num1
+end
+
+def modulus(num1, num2)
+    num1 % num2 
+end
+
+puts "Simple Calculator"
+20.times { print "-"}
+puts 
+
+#1
+puts "Please enter the first number" 
+num_1 = gets.chomp.to_f
+puts "Please enter the second number"
+num_2 = gets.chomp.to_f
+
+#2
+puts "The first number multiplied by the second number is #{multiply(num_1, num_2)}"
+
+#3
+puts "The first number divided by the second number is #{divide(num_1, num_2)}"
+
+#4 
+puts "The fist number subtracted from the second number is #{subtract(num_2, num_1)}"
+
+#5
+puts "The first number added to the second number is #{add(num_2, num_1)}"
+
+#6
+puts "The first number modulus the second number is #{modulus(num_1, num_2)}"
+
