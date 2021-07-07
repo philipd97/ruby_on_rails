ef6779dbceea66b1b28850d7d9df4397658ed7cc
philipd <phildkh97@gmail.com>
2021-07-07 22:18:55 +0800

    fix: handle conversion to float at method

--- section2/methods.rb
+++ section2/methods.rb
@@ -3,31 +3,31 @@
 # Therefore Methods have to be declared above before calling it
 
 def multiply(num_1, num2)
-  num_1 * num2
+  num_1.to_f * num2.to_f
 end
 
 def divide(num1, num2)
-  if num2.eql?(0.0)
-    return "invalid, as #{num1} could not divide by #{num2}" # The first number divided by the second number is invalid, as 1.0 could not divide by 0.0
+  if num2.to_f.eql?(0.0)
+    return "invalid, as #{num1} could not divide by #{num2}" # The first number divided by the second number is invalid, as 1 could not divide by 0
   end
 
-  return num1 / num2
+  return num1.to_f / num2.to_f
 end
 
 def subtract(num2, num1)
-  num2 - num1
+  num2.to_f - num1.to_f
 end
 
 def add(num2, num1)
-  num2 + num1
+  num2.to_f + num1.to_f
 end
 
 def modulus(num1, num2)
-  if num2.eql?(0.0)
-    return "invalid, as #{num1} could not be modulus by #{num2}" # The first number modulus the second number is invalid, as 1.0 could not perform modulus by 0.0
+  if num2.to_f.eql?(0.0)
+    return "invalid, as #{num1} could not be modulus by #{num2}" # The first number modulus the second number is invalid, as 1 could not perform modulus by 0
   end
-  
-  return num1 % num2 
+
+  return num1.to_f % num2.to_f
 end
 
 puts "Simple Calculator"
@@ -36,9 +36,9 @@ puts
 
 #1
 puts "Please enter the first number" 
-num_1 = gets.chomp.to_f
+num_1 = gets.chomp
 puts "Please enter the second number"
-num_2 = gets.chomp.to_f
+num_2 = gets.chomp
 
 #2
 puts "The first number multiplied by the second number is #{multiply(num_1, num_2)}"
