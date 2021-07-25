474c8a79a419f342a7f56086d079a8c1ce4dc1ef
philipd <phildkh97@gmail.com>
2021-07-25 17:03:36 +0800

    refactor: move indentation to 2 space

--- section2/authenticator.rb
+++ section2/authenticator.rb
@@ -6,11 +6,11 @@
 # - only 3 number of attempt available
 
 users = [
-    { username: "user1", password: "password1" },
-    { username: "user2", password: "password2" },
-    { username: "user3", password: "password3" },
-    { username: "user4", password: "password4" },
-    { username: "user5", password: "password5" },
+  { username: "user1", password: "password1" },
+  { username: "user2", password: "password2" },
+  { username: "user3", password: "password3" },
+  { username: "user4", password: "password4" },
+  { username: "user5", password: "password5" },
 ]
 
 def checkUserInput(username, password, arrayHash)
