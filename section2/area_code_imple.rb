71816348e42cabaa728b7bc43f0a1ca14a81a946
philipd <phildkh97@gmail.com>
2021-08-01 20:42:55 +0800

    refactor: remove redundant methods & format if else using ternary operator

--- section2/area_code_imple.rb
+++ section2/area_code_imple.rb
@@ -13,27 +13,24 @@ dial_book = {
   "lancaster" => "717"
 }
 
-def get_city_name(somehash)
-  somehash.keys
-end
+#----==Redundant Methods==----
+# def get_city_name(somehash)
+#   somehash.keys
+# end
 
-def get_area_code(somehash, key)
-  somehash[key]
-end
+# def get_area_code(somehash, key)
+#   somehash[key]
+# end
 
 loop do
   puts "Do you want to lookup an area code based on a city name? (Y/N)"
   answer = gets.chomp.downcase
   break if answer != "y"
   puts "Which city do you want to lookup for the area code for?"
-  puts get_city_name(dial_book)
+  puts dial_book.keys
   puts "Enter your selection"
   prompt = gets.chomp
 
-  if dial_book.include?(prompt)
-    puts "The area code for #{prompt} is #{get_area_code(dial_book, prompt)}"
-  else
-    puts "You entered an invalid city name"
-  end
+  puts dial_book.include?(prompt) ? "The area code for #{prompt} is #{dial_book[prompt]}" : "You entered an invalid city name"
 end
 
