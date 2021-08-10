d1ec6c48c0732ea29676c32649e38853e0584d31
philipd <phildkh97@gmail.com>
2021-08-10 17:33:33 +0800

    feat(crud): add crud module file into section2 folder

---
+++ section2/crud.rb
@@ -0,0 +1,26 @@
+module Crud
+  require 'bcrypt'
+  puts "Module CRUD activated"
+
+  def self.create_hash_digest(password)
+    BCrypt::Password.create(password)
+  end
+
+  def self.verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+
+  def self.create_secure_users(list_of_users)
+    list_of_users.each do |user_record|
+      user_record[:password] = create_hash_digest(user_record[:password])
+    end
+    list_of_users
+  end
+
+  def self.authenticate_user(username, password, list_of_users)
+    list_of_users.find { |user_record|
+      user_record[:username] == username && user_record[:password] == password 
+    } || "Credentials were not correct"
+  end
+end
+
