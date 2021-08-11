f05802749dc3dc2ecfa3ce7815bb093804b06d6e
philipd <phildkh97@gmail.com>
2021-08-11 22:35:58 +0800

    refactor: remove self. on method to cater on include

--- section2/crud.rb
+++ section2/crud.rb
@@ -2,22 +2,26 @@ module Crud
   require 'bcrypt'
   puts "Module CRUD activated"
 
-  def self.create_hash_digest(password)
+  # def self.create_hash_digest(password)
+  def create_hash_digest(password)
     BCrypt::Password.create(password)
   end
 
-  def self.verify_hash_digest(password)
+  # def self.verify_hash_digest(password)
+  def verify_hash_digest(password)
     BCrypt::Password.new(password)
   end
 
-  def self.create_secure_users(list_of_users)
+  # def self.create_secure_users(list_of_users)
+  def create_secure_users(list_of_users)
     list_of_users.each do |user_record|
       user_record[:password] = create_hash_digest(user_record[:password])
     end
     list_of_users
   end
 
-  def self.authenticate_user(username, password, list_of_users)
+  # def self.authenticate_user(username, password, list_of_users)
+  def authenticate_user(username, password, list_of_users)
     list_of_users.find { |user_record|
       user_record[:username] == username && user_record[:password] == password 
     } || "Credentials were not correct"
