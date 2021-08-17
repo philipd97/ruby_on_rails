5aa54ae154c66da8e6aeb905d7bd0d2c4f2817cf
philipd <phildkh97@gmail.com>
2021-08-17 23:14:25 +0800

    refactor: add self. for instance method of Student cl ss & changes of instance method to class method

--- section2/crud.rb
+++ section2/crud.rb
@@ -3,27 +3,26 @@ module Crud
   puts "Module CRUD activated"
 
   # def self.create_hash_digest(password)
-  def create_hash_digest(password)
-    BCrypt::Password.create(password)
+  def create_hash_digest(password = nil)
+    BCrypt::Password.create(respond_to?(:password) ? self.password : password)
   end
 
   # def self.verify_hash_digest(password)
-  def verify_hash_digest(password)
-    BCrypt::Password.new(password)
+  def verify_hash_digest
+    BCrypt::Password.new(self.password)
   end
 
-  # def self.create_secure_users(list_of_users)
-  def create_secure_users(list_of_users)
+  def self.create_secure_users(list_of_users)
     list_of_users.each do |user_record|
-      user_record[:password] = create_hash_digest(user_record[:password])
+      user_record[:password] = Class.include(Crud).create_hash_digest(user_record[:password])
     end
     list_of_users
   end
 
   # def self.authenticate_user(username, password, list_of_users)
-  def authenticate_user(username, password, list_of_users)
+  def authenticate_user(list_of_users = [])
     list_of_users.find { |user_record|
-      user_record[:username] == username && user_record[:password] == password 
+      user_record[:username] == self.username && user_record[:password] == self.password 
     } || "Credentials were not correct"
   end
 end
