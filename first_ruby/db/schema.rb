68ee7590468335f3603f84d4930f387151c75dcc
philipd <phildkh97@gmail.com>
2021-10-23 16:52:08 +0800

    feat(user): generate user scaffold and add migration for add user_id to article

--- first_ruby/db/schema.rb
+++ first_ruby/db/schema.rb
@@ -10,14 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_151507) do
+ActiveRecord::Schema.define(version: 2021_10_23_084034) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "string"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
-
